5d00e1aa2efe213944a0793b4078d7c6eae41148
Scott <scotthenry1@gmail.com>
2023-01-31 12:00:06 -0500

    Completed lab submission

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser = "Scott Henry";
+let welcomeMessage = "Welcome to Flatbook, ";
+
+welcomeMessage = welcomeMessage + currentUser + "!";
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+let shortGreeting = "Welcome, " + firstInitial + "!";
